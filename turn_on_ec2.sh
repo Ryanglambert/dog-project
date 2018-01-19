a725a4166fe6e8fa51efc17436c9f0b57ed8aa7c
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-01-18 17:45:27 -0800

    using larger instsance on aws

--- turn_on_ec2.sh
+++ turn_on_ec2.sh
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-79aeae19 --count 1 --instance-type t2.micro --key-name "ryans key" --security-groups kojak --block-device-mappings file://block_device_mapping.json
+aws ec2 run-instances --image-id ami-79aeae19 --count 1 --instance-type g2.2xlarge --key-name "ryans key" --security-groups kojak --block-device-mappings file://block_device_mapping.json
