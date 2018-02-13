c88cba3872d9ce5232b2823c087a28670e6ca3ca
Ryan Lambert <ryan.g.lambert@gmail.com>
2018-02-13 13:11:32 -0800

    first dl submission

--- turn_on_ec2.sh
+++
@@ -1,1 +0,0 @@
-aws ec2 run-instances --image-id ami-79aeae19 --count 1 --instance-type g2.2xlarge --key-name "ryans key" --security-groups kojak --block-device-mappings file://block_device_mapping.json
